b0e7ed6c53ef00f0511c56b4662cee34e54dff37
Leboxgang <thevirusbruh@gmail.com>
2024-06-28 07:25:13 -0700

    print numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,15 +1,16 @@
 #include "main.h"
+
 /**
  * print numbers - print numbbers from 0-9
  *
- * No return
+ * Return: No return void
  */
 
 void print_numbers(void)
 {
 	int pr;
 
-	for (pr = 48; pr<= 57; pr++)
+	for (pr = 48; pr <= 57; pr++)
 	{
 		_putchar(pr);
 	}
