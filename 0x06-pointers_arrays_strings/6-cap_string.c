1cbb3103219e22032581e5dfe48e774620020c1a
Leboxgang <thevirusbruh@gmail.com>
2024-07-05 03:52:27 -0700

    fixes

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,35 +1,43 @@
 #include "main.h"
 
 /**
- * cap_string - Capitalizes the first letter of each word in a string.
+ * cap_string - Capitalizes all words of a string.
  * @str: The string to be capitalized.
  *
- * Return: A pointer to the modified string.
+ * Return: A pointer to the changed string.
  */
 char *cap_string(char *str)
 {
-	int index = 0;
-	int cap_next = 1;
+    int index = 0;
+    int capitalize_next = 1;
 
-	while (str[index] != '\0')
-	{
-		if (cap_next && (str[index] >= 'a' && str[index] <= 'z'))
-		{
-			str[index] -= 32;
-			cap_next = 0;
-		}
+    while (str[index] != '\0')
+    {
+        if (capitalize_next && (str[index] >= 'a' && str[index] <= 'z'))
+        {
+            str[index] -= 32;
+            capitalize_next = 0;
+        }
 
-		if (str[index] == ' ' || str[index] == '\t' || str[index] == '\n' ||
-		    str[index] == ',' || str[index] == ';' || str[index] == '.' ||
-		    str[index] == '!' || str[index] == '?' || str[index] == '"' ||
-		    str[index] == '(' || str[index] == ')' || str[index] == '{' ||
-		    str[index] == '}')
-		{
-			cap_next = 1;
-		}
+        if (str[index] == ' ' || str[index] == '\t' || str[index] == '\n' ||
+            str[index] == ',' || str[index] == ';' || str[index] == '.' ||
+            str[index] == '!' || str[index] == '?' || str[index] == '"' ||
+            str[index] == '(' || str[index] == ')' || str[index] == '{' ||
+            str[index] == '}')
+        {
+            capitalize_next = 1;
+        }
+        else if (str[index] >= '0' && str[index] <= '9')
+        {
+            capitalize_next = 0;
+        }
+        else
+        {
+            capitalize_next = 0;
+        }
 
-		index++;
-	}
+        index++;
+    }
 
-	return str;
+    return str;
 }
--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,21 +8,21 @@
  */
 char *leet(char *n)
 {
-	int i, j;
-	char s1[] = "aAeEoOtTlL";
-	char s2[] = "4433007711";
+    int i, j;
+    char s1[] = "aAeEoOtTlL";
+    char s2[] = "4433007711";
 
-	for (i = 0; n[i] != '\0'; i++)
-	{
-		for (j = 0; j < 10; j++)
-		{
-			if (n[i] == s1[j])
-			{
-				n[i] = s2[j];
-				break; // Exit the inner loop once a substitution is made
-			}
-		}
-	}
+    for (i = 0; n[i] != '\0'; i++)
+    {
+        for (j = 0; j < 10; j++)
+        {
+            if (n[i] == s1[j])
+            {
+                n[i] = s2[j];
+                break;
+            }
+        }
+    }
 
-	return n;
+    return n;
 }
