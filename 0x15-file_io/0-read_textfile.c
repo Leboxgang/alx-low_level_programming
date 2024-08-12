0eebeedff78a752c7090602a21eed01840019c43
Leboxgang <thevirusbruh@gmail.com>
2024-08-12 15:08:49 -0700

    test

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,40 +1,37 @@
 #include "main.h"
-#include <stdlib.h>
 
 /**
- * read_textfile - Reads a text file and prints it to the POSIX standard output
- * @filename: The text file to be read
- * @letters: The number of letters to read and print
- * 
- * Return: The actual number of bytes read and printed,
- *         0 if the function fails or filename is NULL.
+ * create_file - Creates a file.
+ * @filename: A pointer to the name of the file to create.
+ * @text_content: A pointer to a string to write to the file.
+ *
+ * Return: If the function fails - -1.
+ *         Otherwise - 1.
  */
-ssize_t read_textfile(const char *filename, size_t letters)
+int create_file(const char *filename, char *text_content)
 {
-    char *buf;
-    ssize_t fd;
-    ssize_t w;
-    ssize_t t;
+	int fd, w, len = 0;
 
-    if (filename == NULL)
-        return (0);
+	if (filename == NULL)
+		return (-1);
 
-    fd = open(filename, O_RDONLY);
-    if (fd == -1)
-        return (0);
+	if (text_content != NULL)
+	{
+		while (text_content[len])
+			len++;
+	}
 
-    buf = malloc(sizeof(char) * letters);
-    if (buf == NULL)
-    {
-        close(fd);
-        return (0);
-    }
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
 
-    t = read(fd, buf, letters);
-    w = write(STDOUT_FILENO, buf, t);
+	w = write(fd, text_content, len);
+	if (w == -1)
+	{
+		close(fd);
+		return (-1);
+	}
 
-    free(buf);
-    close(fd);
-
-    return (w);
+	close(fd);
+	return (1);
 }
