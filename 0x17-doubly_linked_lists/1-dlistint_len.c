4e223152280b37980d0c0f842f50bf441ccbcf8f
Leboxgang <thevirusbruh@gmail.com>
2024-09-19 08:38:57 -0700

    8 dif tests

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,14 +1,14 @@
 #include "lists.h"
 
 /**
+ * returns the number of elements in a double linked list
+ *
  * Return: the number of nodes
  */
-size_t print_dlistint(const dlistint_t *h)
+size_t dlistint_len(const dlistint_t *h)
 {
 	int count;
-
 	count = 0;
-
 	if (h == NULL)
 		return (count);
 
@@ -17,7 +17,6 @@ size_t print_dlistint(const dlistint_t *h)
 
 	while (h != NULL)
 	{
-		printf("%d\n", h->n);
 		count++;
 		h = h->next;
 	}
